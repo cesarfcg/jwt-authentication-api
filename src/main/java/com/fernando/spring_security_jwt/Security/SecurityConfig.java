899ee5e18cdefe36cb36a0bffbf2d50920f270c9
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-08-06 14:27:24 -0300

    creating acess control

--- src/main/java/com/fernando/spring_security_jwt/Security/SecurityConfig.java
+++ src/main/java/com/fernando/spring_security_jwt/Security/SecurityConfig.java
@@ -1,18 +1,33 @@
 package com.fernando.spring_security_jwt.Security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpClient;
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()));
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form.permitAll());
         return http.build();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/com/fernando/spring_security_jwt/User/AuthController.java
+++ src/main/java/com/fernando/spring_security_jwt/User/AuthController.java
@@ -1,4 +1,14 @@
 package com.fernando.spring_security_jwt.User;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class AuthController {
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin";
+    }
 }
