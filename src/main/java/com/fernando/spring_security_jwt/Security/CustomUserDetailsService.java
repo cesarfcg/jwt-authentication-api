c2f452014a8e50c0a514bacc905a7b787deec56f
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-08-06 13:53:47 -0300

    creating CustomUserDetailsService

---
+++ src/main/java/com/fernando/spring_security_jwt/Security/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.fernando.spring_security_jwt.Security;
+
+import com.fernando.spring_security_jwt.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService  implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
---
+++ src/main/java/com/fernando/spring_security_jwt/User/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fernando.spring_security_jwt.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
