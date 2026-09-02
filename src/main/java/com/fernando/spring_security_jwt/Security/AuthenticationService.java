df815c33492ccb71b7465705bae13f932d132211
cesar <fernandocesar4141@gmail.com>
2026-09-01 21:06:55 -0300

    configurações iniciais do AuthenticationService

---
+++ src/main/java/com/fernando/spring_security_jwt/Security/AuthenticationService.java
@@ -0,0 +1,18 @@
+package com.fernando.spring_security_jwt.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Service
+public class AuthenticationService {
+    private final JwtService jwtService;
+    @PostMapping("authenticate")
+    public String authenticate(Authentication authentication){
+        return jwtService.generateToken(authentication);
+
+    }
+
+}
