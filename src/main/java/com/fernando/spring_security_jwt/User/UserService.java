1d7aa494e7a3a5e9f7713986c8177d1e6bbbd97f
cesar <fernandocesar4141@gmail.com>
2026-09-13 16:15:49 -0300

    implement findUser for profile

--- src/main/java/com/fernando/spring_security_jwt/User/UserService.java
+++ src/main/java/com/fernando/spring_security_jwt/User/UserService.java
@@ -2,6 +2,7 @@ package com.fernando.spring_security_jwt.User;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,5 +15,15 @@ public class UserService {
 
     public List<User> list() {
         return userRepository.findAll();
+    }
+    public UserResponseDto findUser(Authentication authentication){
+        String email = authentication.getName();
+        User user = userRepository.findByUsername(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return new UserResponseDto(
+                user.getId(),
+                user.getUsername(),
+                user.getRole()
+        );
     }
 }
